f3cb0a44d541a7b8cb9d1dccb72e6c19e2c3c08f
João Bezerra <joaobzzr@gmail.com>
2024-03-05 20:17:33 -0300

    Add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import devtools from 'solid-devtools/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/paladins-randomizer/",
   plugins: [
     devtools({ autoname: true }),
     solid(),
