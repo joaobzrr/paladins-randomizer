75a7c37441d9a202de59f40c12d921db6d82f532
João Bezerra <joaobzzr@gmail.com>
2024-03-06 04:21:43 -0300

    Update vite config to deploy on vercel

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import devtools from 'solid-devtools/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/paladins-randomizer/",
   plugins: [
     devtools({ autoname: true }),
     solid(),
